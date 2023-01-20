3a9ecbdaeb703fbff5996c532c9bd38d0721f565
dylanquaale <dylanquaale33@gmail.com>
2023-01-20 11:19:49 -0600

    server packagers added

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 // get the client
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+
+const PORT = process.env.PORT || 3001;
 
 const connection = mysql.createConnection({
     host: 'localhost',
