79bf0f98f7cdcd113eb51e43691a48176e9203aa
dylanquaale <dylanquaale33@gmail.com>
2023-01-20 11:33:28 -0600

    department added on schema.sql

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 // get the client
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-const cTable = require('console.table');
+    require('console.table');
 
-const PORT = process.env.PORT || 3001;
 
 const connection = mysql.createConnection({
     host: 'localhost',
