616f2e7829fce6d437901143876cacaf98ee27d6
dylanquaale <dylanquaale33@gmail.com>
2023-01-20 11:40:10 -0600

    SCHEMA.SQL CREATED

--- schema.sql
+++ schema.sql
@@ -9,3 +9,20 @@ CREATE TABLE department (
     PRIMARY KEY (id)
 );
 
+CREATE TABLE rolE (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(20.4) NULL,
+    department_id INT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id)
+);
